9091ea13c913b34e3c2e6dc458ff269a87897a96
Victor <victoraguilerag17@gmail.com>
2022-08-28 20:48:50 -0300

    Add List components

---
+++ src/components/List/ListContainer.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { mapActions, mapState } from "vuex";
+  import ListItem from "./ListItem.vue";
+  export default {
+    computed: {
+      ...mapState({
+        foundBooks: (state) => state.books.searchResults,
+        books: (state) => state.books.items,
+        foundCities: (state) => state.cities.searchResults,
+        cities: (state) => state.cities.items,
+      }),
+    },
+    methods: {
+      ...mapActions("books", ["search"]),
+    },
+    components: {
+      ListItem,
+    },
+  };
+</script>
+
+<template>
+  <ul class="listContainer">
+    <ListItem
+      v-for="book in foundBooks"
+      v-bind:key="book.title"
+      :text="book.title"
+      type="book"
+    />
+    <ListItem
+      v-for="city in foundCities"
+      v-bind:key="city"
+      :text="city"
+      type="city"
+    />
+  </ul>
+</template>
+
+<style scoped>
+  .listContainer {
+    list-style: none;
+    padding: 0;
+  }
+</style>
---
+++ src/components/List/ListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="listItem">
+    <span>{{ text }}</span>
+    <span class="type">{{ type }}</span>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: {
+      text: String,
+      type: String,
+    },
+  };
+</script>
+
+<style scoped>
+  .listItem {
+    height: 48px;
+    background: whitesmoke;
+    padding: 8px;
+    font-weight: 400;
+    border: 1px solid rgba(200, 200, 200, 0.1);
+    display: flex;
+    justify-content: space-between;
+  }
+  .type {
+    font-weight: bold;
+    margin-right: 16px;
+  }
+</style>
