c9c7b68d10432ea1a7b0fcf8834f8dbd667f6d2b
Victor <victoraguilerag17@gmail.com>
2022-08-28 20:45:44 -0300

    Add Mocks

---
+++ src/mocks/data.js
@@ -0,0 +1,34 @@
+const cities = [
+    'san jose', 'santiago', 'san francisco', 'santa rosa', 'san juan', 'sabadell', 'salamanca', 'salt lake city', 'salinas', 'salem', 'sausalito', 'taipei', 'tel aviv', 'tempe', 'termez', 'temuco', 'tiajuna', 'tieling', 'thousand oaks', 'thunder bay', 'tokyo', 'tulsa'
+  ]
+  
+  const books = [
+    {
+      title: 'Don Quixote',
+      author: 'Miguel De Cervantes',
+    },
+    {
+      title: 'Pilgrim\'s Progress',
+      author: 'John Bunyan',
+    },
+    {
+      title: 'Robinson Crusoe',
+      author: 'Daniel Defoe',
+    },
+    {
+      title: 'Gulliver\'s Travels',
+      author: 'Jonathan Swift',
+    },
+    {
+      title: 'Tom Jones',
+      author: 'Henry Fielding',
+    },
+    {
+      title: 'Clarissa',
+      author: 'Samuel Richardson',
+    },
+    {
+      title: 'Tristram Shandy',
+      author: 'Laurence Sterne',
+    },
+  ]
